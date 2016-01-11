1259ceb9a6f53426ba04dbf5157b94e0cbea68c7
Ezequiel Santamaría Navarro <ezequiel@umu.ezequiel>
2016-01-11 16:11:16 +0100

    En registration.js, se añade el filtro isNotAlreadyMapped para no registrar dos veces el mismo dispositivo.
    En ngsiHandlers.js, se cambia 'prueba' por commandResult.
    
    Añado en downloadOmaRegistry código para que añada también la información de operation.
    
    Modifico todo el código para que incluya omaInverseRegistry en vez de omaRegistryInverseMap.
    
    En ngsiHandlers, cuando se hace una petición de lectura, filtro todo atributo que no tenga "R" en operation.
    
    En commonLwm2m corrijo un problema que no permite observar atributos que no estén explícitamente
    programados en el internalAttributes.

--- config.js
+++ config.js
@@ -24,51 +24,66 @@
 var config = {};
 
 config.lwm2m = {
-    logLevel: 'DEBUG',
-    port: 60001,
-    defaultType: 'Device',
-    ipProtocol: 'udp4',
-    serverProtocol: 'udp4',
-    formats: [
-        {
-            name: 'application-vnd-oma-lwm2m/text',
-            value: 1541
-        },
-        {
-            name: 'application-vnd-oma-lwm2m/tlv',
-            value: 1542
-        },
-        {
-            name: 'application-vnd-oma-lwm2m/json',
-            value: 1543
-        },
-        {
-            name: 'application-vnd-oma-lwm2m/opaque',
-            value: 1544
-        }
-    ],
-    writeFormat: 'application-vnd-oma-lwm2m/text',
-    types: [ ]
+  logLevel: 'DEBUG',
+  port: 5683,
+  defaultType: 'Device',
+  ipProtocol: 'udp6',
+  serverProtocol: 'udp6',
+  formats: [{
+    name: 'application-vnd-oma-lwm2m/text',
+    value: 1541
+  }, {
+    name: 'application-vnd-oma-lwm2m/tlv',
+    value: 1542
+  }, {
+    name: 'application-vnd-oma-lwm2m/json',
+    value: 1543
+  }, {
+    name: 'application-vnd-oma-lwm2m/opaque',
+    value: 1544
+  }],
+  writeFormat: 'application-vnd-oma-lwm2m/text',
+  types: [{
+    name: "IPex16",
+    url: "/ipex16"
+  },
+  {
+    name: "Robot",
+    url: "/robot"
+  },{
+    name: "Device",
+    url: "/"
+  }]
 };
 
 config.ngsi = {
-    logLevel: 'DEBUG',
-    contextBroker: {
-        host: '192.168.56.101',
-        port: '1026'
-    },
-    server: {
-        port: 4041
-    },
-    deviceRegistry: {
-        type: 'mongodb',
- 	host: 'localhost'
-    },
-    types: { },
-    service: 'smartGondor',
-    subservice: '/gardens',
-    providerUrl: 'http://192.168.56.1:4041',
-    deviceRegistrationDuration: 'P1M'
+  logLevel: 'DEBUG',
+  contextBroker: {
+    host: '192.168.56.2',
+    port: '1026'
+  },
+  server: {
+    port: 4041
+  },
+  deviceRegistry: {
+    type: 'mongodb',
+    host: 'localhost'
+  },
+  types: {
+    'Device' : {
+      "service": "odins",
+      "subservice": "/",
+      "removeSuffix": true,
+      "active": [],
+      "lazy": [],
+      "commands": [],
+      "lwm2mResourceMapping": {}
+    }
+  },
+  service: 'odins',
+  subservice: '/',
+  providerUrl: 'http://192.168.56.1:4041',
+  deviceRegistrationDuration: 'P1M'
 };
 
 module.exports = config;
