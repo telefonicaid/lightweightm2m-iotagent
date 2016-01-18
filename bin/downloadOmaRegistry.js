eedde6601ec8ab6eb02b16f27d8d2eae98a10ec4
Ezequiel Santamaría Navarro <ezequiel@umu.ezequiel>
2016-01-18 16:15:58 +0100

    Changed 2-soft indentation to 4-soft indentation.

--- bin/downloadOmaRegistry.js
+++ bin/downloadOmaRegistry.js
@@ -24,156 +24,156 @@
  */
 
 var cheerio = require('cheerio'),
-  async = require('async'),
-  fs = require('fs'),
-  request = require('request'),
-  errors = require('../lib/errors'),
-  logger = require('logops'),
-  context = {
-    op: 'IOTAgent.OMARegistry'
-  },
-  registryTargetFile = 'omaRegistry.json',
-  inverseRegistryTargetFile = 'omaInverseRegistry.json',
-  DOMParser = require('xmldom').DOMParser,
-  _ = require('underscore');
+    async = require('async'),
+    fs = require('fs'),
+    request = require('request'),
+    errors = require('../lib/errors'),
+    logger = require('logops'),
+    context = {
+        op: 'IOTAgent.OMARegistry'
+    },
+    registryTargetFile = 'omaRegistry.json',
+    inverseRegistryTargetFile = 'omaInverseRegistry.json',
+    DOMParser = require('xmldom').DOMParser,
+    _ = require('underscore');
 
 
 function downloadRegistry(url) {
-  return function(callback) {
-    var options = {
-      uri: url,
-      method: 'GET'
-    };
+    return function(callback) {
+        var options = {
+            uri: url,
+            method: 'GET'
+        };
 
-    request(options, function(error, response, body) {
-      if (error) {
-        logger.error(context, 'Couldn\'t retrieve OMA Registry due to a connection error: ' + error);
-        callback(new errors.OmaRegistryConnectionError(error));
-      } else if (response.statusCode === 200) {
-        callback(null, body);
-      } else {
-        callback(new errors.OmaRegistryServerError(response.statusCode));
-      }
-    });
-  };
+        request(options, function(error, response, body) {
+            if (error) {
+                logger.error(context, 'Couldn\'t retrieve OMA Registry due to a connection error: ' + error);
+                callback(new errors.OmaRegistryConnectionError(error));
+            } else if (response.statusCode === 200) {
+                callback(null, body);
+            } else {
+                callback(new errors.OmaRegistryServerError(response.statusCode));
+            }
+        });
+    };
 }
 
 function parseRegistry(registryData, callback) {
-  var $ = cheerio.load(registryData),
-    initiateData = false,
-    registry = [];
+    var $ = cheerio.load(registryData),
+        initiateData = false,
+        registry = [];
 
-  $('tr').each(function(i, elem) {
-    if (!initiateData && $.html(this).indexOf('URN') > 0) {
-      logger.info('Initiating data parse.');
-      initiateData = true;
-    } else if (initiateData) {
-      if ($($(this).find('td').get(1)).text().indexOf('urn') >= 0) {
-        var obj = {
-          name: $($(this).find('td').get(2)).text(),
-          urn: $($(this).find('td').get(1)).text(),
-          id: $($(this).find('td').get(0)).text(),
-          schema: $($(this).find('td').get(3)).find('a').attr('href')
-        };
+    $('tr').each(function(i, elem) {
+        if (!initiateData && $.html(this).indexOf('URN') > 0) {
+            logger.info('Initiating data parse.');
+            initiateData = true;
+        } else if (initiateData) {
+            if ($($(this).find('td').get(1)).text().indexOf('urn') >= 0) {
+                var obj = {
+                    name: $($(this).find('td').get(2)).text(),
+                    urn: $($(this).find('td').get(1)).text(),
+                    id: $($(this).find('td').get(0)).text(),
+                    schema: $($(this).find('td').get(3)).find('a').attr('href')
+                };
 
-        if (obj.name && obj.id) {
-          registry.push(obj);
+                if (obj.name && obj.id) {
+                    registry.push(obj);
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  callback(null, registry);
+    callback(null, registry);
 }
 
 function discoverResources(registryObj, callback) {
-  function retrieveResources(item, callback) {
-    if (item.schema) {
-      downloadRegistry(item.schema)(function(error, body) {
-        if (error) {
-          callback(error);
+    function retrieveResources(item, callback) {
+        if (item.schema) {
+            downloadRegistry(item.schema)(function(error, body) {
+                if (error) {
+                    callback(error);
+                } else {
+                    var doc = new DOMParser().parseFromString(body),
+                        items = doc.getElementsByTagName('Item'),
+                        newObj = _.clone(item);
+
+                    newObj.resources = [];
+                    for (var i = 0; i < items.length; i++) {
+                        var resource = {
+                            name: items[i].getElementsByTagName('Name')[0].textContent,
+                            type: items[i].getElementsByTagName('Type')[0].textContent,
+                            operations: items[i].getElementsByTagName('Operations')[0].textContent
+                        };
+
+                        newObj.resources.push(resource);
+                    }
+                    callback(null, newObj);
+                }
+            });
         } else {
-          var doc = new DOMParser().parseFromString(body),
-            items = doc.getElementsByTagName('Item'),
-            newObj = _.clone(item);
-
-          newObj.resources = [];
-          for (var i = 0; i < items.length; i++) {
-            var resource = {
-              name: items[i].getElementsByTagName('Name')[0].textContent,
-              type: items[i].getElementsByTagName('Type')[0].textContent,
-              operations: items[i].getElementsByTagName('Operations')[0].textContent
-            };
-
-            newObj.resources.push(resource);
-          }
-          callback(null, newObj);
+            callback(null, item);
         }
-      });
-    } else {
-      callback(null, item);
     }
-  }
 
-  async.map(registryObj, retrieveResources, callback);
+    async.map(registryObj, retrieveResources, callback);
 }
 
 function createInternalMap(registryObj, callback) {
-  function createMapWithIds(previous, current) {
-    previous[current.id] = current;
+    function createMapWithIds(previous, current) {
+        previous[current.id] = current;
 
-    return previous;
-  }
-
-  function createMapFromIds(previous, current) {
-    if (current.resources) {
-      for (var i = 0; i < current.resources.length; i++) {
-        previous[current.resources[i].name] = {
-          objectResource: i,
-          objectType: current.id,
-          objectInstance: 0,
-          operations: current.resources[i].operations
-        };
-      }
-    } else {
-      previous[current.name] = {
-        objectResource: 0,
-        objectType: current.id,
-        objectInstance: null
-      };
+        return previous;
     }
 
-    return previous;
-  }
+    function createMapFromIds(previous, current) {
+        if (current.resources) {
+            for (var i = 0; i < current.resources.length; i++) {
+                previous[current.resources[i].name] = {
+                    objectResource: i,
+                    objectType: current.id,
+                    objectInstance: 0,
+                    operations: current.resources[i].operations
+                };
+            }
+        } else {
+            previous[current.name] = {
+                objectResource: 0,
+                objectType: current.id,
+                objectInstance: null
+            };
+        }
 
-  var directRegistryMap = registryObj.reduce(createMapWithIds, {}),
-    inverseRegistryMap = registryObj.reduce(createMapFromIds, {});
+        return previous;
+    }
 
-  callback(null, JSON.stringify(directRegistryMap, null, 4), JSON.stringify(inverseRegistryMap, null, 4));
+    var directRegistryMap = registryObj.reduce(createMapWithIds, {}),
+        inverseRegistryMap = registryObj.reduce(createMapFromIds, {});
+
+    callback(null, JSON.stringify(directRegistryMap, null, 4), JSON.stringify(inverseRegistryMap, null, 4));
 }
 
 function writeResults(directMappings, inverseMappings, callback) {
-  async.series([
-    async.apply(fs.writeFile, registryTargetFile, directMappings),
-    async.apply(fs.writeFile, inverseRegistryTargetFile, inverseMappings)
-  ], callback);
+    async.series([
+        async.apply(fs.writeFile, registryTargetFile, directMappings),
+        async.apply(fs.writeFile, inverseRegistryTargetFile, inverseMappings)
+    ], callback);
 }
 
 function processRegistry(callback) {
-  async.waterfall([
-    downloadRegistry('http://technical.openmobilealliance.org' +
-      '/Technical/technical-information/omna/lightweight-m2m-lwm2m-object-registry'),
-    parseRegistry,
-    discoverResources,
-    createInternalMap,
-    writeResults
-  ], callback);
+    async.waterfall([
+        downloadRegistry('http://technical.openmobilealliance.org' +
+            '/Technical/technical-information/omna/lightweight-m2m-lwm2m-object-registry'),
+        parseRegistry,
+        discoverResources,
+        createInternalMap,
+        writeResults
+    ], callback);
 }
 
 processRegistry(function(error) {
-  if (error) {
-    console.log('Download OMA Registry failed: ' + error);
-  } else {
-    console.log('OMA Registry successfully downloaded');
-  }
+    if (error) {
+        console.log('Download OMA Registry failed: ' + error);
+    } else {
+        console.log('OMA Registry successfully downloaded');
+    }
 });
--- config.js
+++ config.js
@@ -24,66 +24,83 @@
 var config = {};
 
 config.lwm2m = {
-  logLevel: 'DEBUG',
-  port: 5683,
-  defaultType: 'Device',
-  ipProtocol: 'udp6',
-  serverProtocol: 'udp6',
-  formats: [{
-    name: 'application-vnd-oma-lwm2m/text',
-    value: 1541
-  }, {
-    name: 'application-vnd-oma-lwm2m/tlv',
-    value: 1542
-  }, {
-    name: 'application-vnd-oma-lwm2m/json',
-    value: 1543
-  }, {
-    name: 'application-vnd-oma-lwm2m/opaque',
-    value: 1544
-  }],
-  writeFormat: 'application-vnd-oma-lwm2m/text',
-  types: [{
-    name: "IPex16",
-    url: "/ipex16"
-  },
-  {
-    name: "Robot",
-    url: "/robot"
-  },{
-    name: "Device",
-    url: "/"
-  }]
+    logLevel: 'DEBUG',
+    port: 5683,
+    defaultType: 'Device',
+    ipProtocol: 'udp6',
+    serverProtocol: 'udp6',
+    formats: [{
+        name: 'application-vnd-oma-lwm2m/text',
+        value: 1541
+    }, {
+        name: 'application-vnd-oma-lwm2m/tlv',
+        value: 1542
+    }, {
+        name: 'application-vnd-oma-lwm2m/json',
+        value: 1543
+    }, {
+        name: 'application-vnd-oma-lwm2m/opaque',
+        value: 1544
+    }],
+    writeFormat: 'application-vnd-oma-lwm2m/text',
+    types: [{
+        name: "IPex16",
+        url: "/ipex16"
+    }, {
+        name: "Robot",
+        url: "/robot"
+    }, {
+        name: "Device",
+        url: "/"
+    }]
 };
 
 config.ngsi = {
-  logLevel: 'DEBUG',
-  contextBroker: {
-    host: '192.168.56.2',
-    port: '1026'
-  },
-  server: {
-    port: 4041
-  },
-  deviceRegistry: {
-    type: 'mongodb',
-    host: 'localhost'
-  },
-  types: {
-    'Device' : {
-      "service": "odins",
-      "subservice": "/",
-      "removeSuffix": true,
-      "active": [],
-      "lazy": [],
-      "commands": [],
-      "lwm2mResourceMapping": {}
-    }
-  },
-  service: 'odins',
-  subservice: '/',
-  providerUrl: 'http://192.168.56.1:4041',
-  deviceRegistrationDuration: 'P1M'
+    logLevel: 'DEBUG',
+    contextBroker: {
+        host: '155.54.205.185',
+        port: '1026'
+    },
+    server: {
+        port: 4041
+    },
+    deviceRegistry: {
+        type: 'mongodb',
+        host: 'localhost'
+    },
+    types: {
+        'Device': {
+            "service": "odins",
+            "subservice": "/",
+            "removeSuffix": true,
+            "active": [{
+                "name": "Boton",
+                "type": "Integer"
+            }],
+            "lazy": [{
+                "name": "Led",
+                "type": "Integer"
+            }],
+            "commands": [],
+            "lwm2mResourceMapping": {
+                "Boton": {
+                    "objectType": 60000,
+                    "objectInstance": 0,
+                    "objectResource": 0
+                },
+                "Led": {
+                    "objectType": 60000,
+                    "objectInstance": 0,
+                    "objectResource": 1
+                }
+
+            }
+        }
+    },
+    service: 'odins',
+    subservice: '/',
+    providerUrl: 'http://155.54.205.214:4041',
+    deviceRegistrationDuration: 'P1M'
 };
 
 module.exports = config;
