4c9aa8694d0bb07ceedcb91c879da5f8b607c7f9
Daniel Calvo <daniel.calvo@atos.net>
2018-05-17 14:45:34 +0200

    Cleans unneeded changes

--- test/unit/active-attributes-test.js
+++ test/unit/active-attributes-test.js
@@ -87,6 +87,7 @@ describe('Active attributes test', function() {
                 setTimeout(function() {
                     ngsiClient.query('ActiveTestClient:Pressure', 'Pressure', ['pressure'],
                         function(error, response, body) {
+
                             should.not.exist(error);
                             should.exist(body);
                             should.not.exist(body.errorCode);
