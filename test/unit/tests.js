113cdca18de41843f2fe5078228814a8f735adc1
Jason Fox <jason.fox@fiware.org>
2021-04-12 19:20:51 +0200

    Remove tests

--- test/unit/tests.js
+++ test/unit/tests.js
@@ -1,6 +1,1 @@
-require('./registration-test.js');
-require('./update-registration-test.js');
 require('./configuration-test.js');
-require('./active-attributes-test.js');
-require('./passive-attributes-test.js');
-require('./commands-test.js');
